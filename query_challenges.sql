641ca84f83a179394830315ee9be33534474cf06
nevicnevic <neptaliaaron@gmail.com>
2026-03-26 19:11:11 -0500

    file rename and format template

---
+++ query_challenges.sql
@@ -0,0 +1,32 @@
+-- Active: 1774569236320@@_@3307@hr_block_sql_capstone_project
+-- Table describing
+DESC employees;
+DESC departments;
+DESC divisions;
+DESC job_family_titles;
+DESC unions;
+DESC employee_jobs;
+DESC jobs;
+DESC levels;
+DESC job_supervisors;
+DESC job_metadata;
+DESC employee_payments;
+DESC change_types;
+
+-- List all employees currently working in IT, along with their division and job title.
+
+-- Show the complete organizational structure of Finance – list each position and to whom it reports (if any).
+
+-- Among all employees who have never changed position, find who has worked the longest.
+
+-- Calculate the current pay of 'Sarah Johnson', factoring in her years of service and the pay rules of her current position.
+
+-- Find all vacant positions (positions with no one assigned to them).
+
+-- List all employees who have been promoted at least twice (each time to a job that supervised their previous one).
+
+-- List all employees that report (directly or indirectly) to 'Inderjeet Singh', the current Director of IT.
+
+-- For Inderjeet Singh, Director of IT, list all his previous positions and the pay he was receiving on his last day in those positions.
+
+-- Group employees by their union (or lack thereof) and list the results in descending order of count.
