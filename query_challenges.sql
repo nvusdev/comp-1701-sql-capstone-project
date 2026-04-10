9dcda7268289fc49befc3776e1a64f893f85453f
nevicnevic <neptaliaaron@gmail.com>
2026-04-10 13:05:56 -0500

    partial completion of query challenges; need reasoning

--- query_challenges.sql
+++ query_challenges.sql
@@ -1,4 +1,4 @@
--- Active: 1774622008260@@gondola.proxy.rlwy.net@30240@hr_block_sql_capstone_projectk_sql_capstone_project
+-- Active: 1774622008260@@gondola.proxy.rlwy.net@30240@hr_block_sql_capstone_project
 -- Table describing
 DESC employees;
 DESC departments;
@@ -56,12 +56,63 @@ LIMIT 1;
 
 -- Calculate the current pay of 'Sarah Johnson', factoring in her years of service and the pay rules of her current position.
 
+SELECT * FROM employees
+WHERE first_name = 'Sarah';
+
 -- Find all vacant positions (positions with no one assigned to them).
+
+SELECT 
+    j.job_id,
+    j.job_title
+FROM jobs j
+LEFT JOIN employee_jobs ej ON j.job_id = ej.job_id AND ej.end_date IS NULL
+WHERE ej.employee_job_id IS NULL
+ORDER BY j.job_title;
 
 -- List all employees who have been promoted at least twice (each time to a job that supervised their previous one).
 
+SELECT
+    e.first_name,
+    e.last_name,
+    COUNT(*) AS number_of_promotions
+FROM employees e
+JOIN employee_jobs ej
+    ON e.employee_id = ej.employee_id
+WHERE ej.change_type_id = (SELECT change_type_id FROM change_types WHERE change_type_name = 'Promotion')
+GROUP BY e.employee_id
+HAVING COUNT(*) >= 2;
+
 -- List all employees that report (directly or indirectly) to 'Inderjeet Singh', the current Director of IT.
 
--- For Inderjeet Singh, Director of IT, list all his previous positions and the pay he was receiving on his last day in those positions.
+
+
+-- Where employee_id = (
+--     select e.employee_id
+--     from employees e
+--     join employee_jobs ej on e.employee_id = ej.employee_id
+--     join jobs j on ej.job_id = j.job_id
+--     where j.job_title = 'Director — IT'
+-- )
+
+-- For Director of IT, list all his previous positions and the pay he was receiving on his last day in those positions.
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+e.employee_id,
+j.job_title,
+ej.pay_scale,
+ej.end_date
+FROM employees e
+JOIN employee_jobs ej ON e.employee_id = ej.employee_id
+JOIN jobs j ON ej.job_id = j.job_id -- works
 
 -- Group employees by their union (or lack thereof) and list the results in descending order of count.
+
+SELECT 
+    u.union_name,
+    COUNT(e.employee_id) AS employee_count
+FROM employees e
+JOIN employee_jobs ej ON e.employee_id = ej.employee_id
+JOIN jobs j ON ej.job_id = j.job_id
+LEFT JOIN unions u ON j.union_id = u.union_id
+GROUP BY u.union_name
+ORDER BY employee_count DESC;
